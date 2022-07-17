c1f83174da4837a5e0628e1aa5e3ca8001a9ae66
PURUSHOTHAM-REDDY-N <84611950+PURUSHOTHAM-REDDY-N@users.noreply.github.com>
2022-07-17 18:24:48 +0530

    Add files via upload

---
+++ tmklink/script.js
@@ -0,0 +1,39 @@
+let apitok = "90958f496c499bfae34829a3ca1edb3fd8e62ffb";
+let show = document.getElementById("link");
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+    let url = tabs[0].url;
+    // use `url` here inside the callback because it's asynchronous!
+
+    fetch(`https://tmklink.com/api?api=${apitok}&url=${url}`)
+        .then(data => data.json())
+        .then(linkData => {
+            const linkText = linkData.shortenedUrl;
+            
+            if (linkData.status === "error") {
+                show.innerHTML = url;
+            }
+            else {
+                show.innerText = linkText;
+            }
+
+            
+
+        });
+});
+
+let btnlink=document.getElementById("btn-link");
+let cp=document.getElementById("cp");
+
+cp.addEventListener("click",()=>{
+    
+
+    show.style.color="red";
+    navigator.clipboard.writeText(show.innerText);
+    
+})
+
+
+
+
+
+
